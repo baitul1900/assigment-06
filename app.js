83680cc05f22876bac03fddcbb5fef20fe5af7d1
Antu <baitulamin1900@gmail.com>
2023-09-01 23:52:37 +0600

    updated

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 const express = require('express');
+const app = express();
+const router = require('./src/routers/api');
+
+app.use('/api' , router);
+
+
+
+
+
+
+
+module.exports = app; 
--- index.js
+++ index.js
@@ -4,3 +4,4 @@ const port  = 8080;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+

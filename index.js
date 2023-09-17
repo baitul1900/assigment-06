06780da8d7f97d55f17692e7295121d742911c5b
Antu <baitulamin1900@gmail.com>
2023-09-17 20:32:34 +0600

    done

--- index.js
+++ index.js
@@ -5,36 +5,72 @@ app.listen(port, () => {
   console.log(``);
 });
 
-const celciusToFahrenheit = (input) => {
-  const fahrenheit = input * (9 / 5) + 32;
-  //   const y = Math.round(fahrenheit * 100) / 100;
-  return fahrenheit;
-};
+// const celciusToFahrenheit = (input) => {
+//   const fahrenheit = input * (9 / 5) + 32;
+//   //   const y = Math.round(fahrenheit * 100) / 100;
+//   return fahrenheit;
+// };
 
-console.log(celciusToFahrenheit(55.125478962));
+// console.log(celciusToFahrenheit(55.125478962));
 
-const arr = [1, 2, 2, 3, 1, 45];
-const [a, b] = arr;
+// const arr = [1, 2, 2, 3, 1, 45];
+// const [a, b] = arr;
 
-console.log(a);
+// console.log(a);
 
-// find the square of a number
+// // find the square of a number
 
-const square = (sqt) => {
-  const sq = Math.pow(sqt, 2);
-  return sq;
-};
+// const square = (sqt) => {
+//   const sq = Math.pow(sqt, 2);
+//   return sq;
+// };
 
-console.log(square(12));
+// console.log(square(12));
 
-// find the value of x here
+// // find the value of x here
+
+// // function factorial(n) {
+// //   let answer = 1;
+// //   if (n == 0 || n == 1) {
+// //     return answer;
+// //   } else if (n > 1) {
+// //     for (let i = n; (i) => 1; i--) {
+// //       answer = answer * i;
+// //     }
+// //     return answer;
+// //   } else {
+// //     return "number has to be positive";
+// //   }
+// // }
+
+// // let n = 4;
+// // answer = factorial(n);
+// // console.log(`Factorial of " + ${n} + " : " + ${answer}`);
 
 // function factorial(n) {
 //   let answer = 1;
 //   if (n == 0 || n == 1) {
 //     return answer;
 //   } else if (n > 1) {
-//     for (let i = n; (i) => 1; i--) {
+//     for (let i = n; i >= 1; i--) {
+//       answer = answer * i;
+//     }
+//     return answer;
+//   } else {
+//     return "number has to be positive.";
+//   }
+// }
+// let c = 6;
+// answer = factorial(c);
+// console.log("Factorial of " + c + " : " + answer);
+
+// // new here
+// function secondFactorial(theFoctorialNumber) {
+//   let answer = 1;
+//   if (theFoctorialNumber == 0 || theFoctorialNumber == 1) {
+//     return answer;
+//   } else if (theFoctorialNumber > 1) {
+//     for (let i = theFoctorialNumber; i >= 1; i--) {
 //       answer = answer * i;
 //     }
 //     return answer;
@@ -43,42 +79,27 @@ console.log(square(12));
 //   }
 // }
 
-// let n = 4;
-// answer = factorial(n);
-// console.log(`Factorial of " + ${n} + " : " + ${answer}`);
+// let theNumber = 8;
+// answer = secondFactorial(theNumber);
+// console.log("Factorial of " + theNumber + " : " + answer);
 
-function factorial(n) {
-  let answer = 1;
-  if (n == 0 || n == 1) {
-    return answer;
-  } else if (n > 1) {
-    for (let i = n; i >= 1; i--) {
-      answer = answer * i;
-    }
-    return answer;
-  } else {
-    return "number has to be positive.";
-  }
-}
-let c = 6;
-answer = factorial(c);
-console.log("Factorial of " + c + " : " + answer);
+// find out the parcentage of how much a 1 usd is woth how much chocoloate
 
-// new here
-function secondFactorial(theFoctorialNumber) {
-  let answer = 1;
-  if (theFoctorialNumber == 0 || theFoctorialNumber == 1) {
-    return answer;
-  } else if (theFoctorialNumber > 1) {
-    for (let i = theFoctorialNumber; i >= 1; i--) {
-      answer = answer * i;
-    }
-    return answer;
-  } else {
-    return "number has to be positive";
-  }
+function calculatePercentage(
+  amount,
+  perChocolate,
+  lossParcentage,
+  expectedProfit_parcentage
+) {
+  let allParcentage = 100;
+  let oneChocolatePrice = amount / perChocolate;
+  let lossInsale = 100 - lossParcentage;
+  let sellpriceAt = allParcentage / lossInsale;
+  let oldPriceSell = sellpriceAt * oneChocolatePrice;
+  let profitSalling = allParcentage + expectedProfit_parcentage;
+  let newsoldPrice = (profitSalling / allParcentage) * oldPriceSell;
+  let profitMarginPice = amount / newsoldPrice;
+  return `So, ${expectedProfit_parcentage}% can be achive if we get ${profitMarginPice} chocolate in tk ${amount}.`;
 }
 
-let theNumber = 8;
-answer = secondFactorial(theNumber);
-console.log("Factorial of " + theNumber + " : " + answer);
+console.log(calculatePercentage(1, 4, 10, 20));
